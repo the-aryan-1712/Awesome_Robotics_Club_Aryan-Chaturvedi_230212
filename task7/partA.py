40a8a9a30679b1620fceb83b38ea1f1f5ed96478
the-aryan-1712 <156575544+the-aryan-1712@users.noreply.github.com>
2024-04-09 02:04:26 +0530

    Update partA.py

--- task7/partA.py
+++ task7/partA.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 
-def find_black_blocks(image, intensity):
+def find_black_rows(image, intensity):
     black_indices = []
     for i in range(image.shape[0]):
         avg = np.mean(image[i], axis=0)
         if np.average(avg) < intensity:
             black_indices.append(i)
-            image[i] = [0, 0, 0]
+            for j in range(image.shape[1]):
+                
+                if not np.allclose(image[i][j], [255, 255, 255], atol=10):
+                    image[i, j] = [0, 0, 0]           
+
     return black_indices
 
 def find_black_columns(image, intensity):
@@ -16,38 +20,30 @@ def find_black_columns(image, intensity):
         avg = np.mean(image[:, i], axis=0)
         if np.average(avg) < intensity:
             black_indices.append(i)
-            image[:, i] = [0, 0, 0]
+            for j in range(image.shape[0]):
+                
+                if not np.allclose(image[j][i], [255, 255, 255], atol=10):
+                    image[j, i] = [0, 0, 0] 
+    
     return black_indices
 
-def process_grid_blocks(image, black_rows, black_cols, intensity):
-    rows = len(black_rows) - 1
-    cols = len(black_cols) - 1
-    grid = []
-    for i in range(rows):
-        row_arr = []
+def process_grid_blocks(image, black_rows, black_cols):
+    rows = len(black_rows)-1 
+    cols = len(black_cols)-1
+    for i in range(rows): 
         for j in range(cols):
             block = image[black_rows[i]+1:black_rows[i+1], black_cols[j]+1:black_cols[j+1]]
             avg_color = np.mean(block, axis=(0, 1))
-            if avg_color[0] > intensity:
-                row_arr.append(1)
-                block[:, :] = [255, 0, 0]
-            else:
-                row_arr.append(0)
-                block[:, :] = [0, 255, 255]
-        grid.append(row_arr)
-    return grid
+            block[:, :] = avg_color
+
+# Input for image
+image = cv2.imread("sample_input.png")
 
 
-# Read the image
-image = cv2.imread("Screenshot 2024-04-08 205039.png")
-# Find black rows and columns
-
-black_row_indices = find_black_blocks(image, 94)
+black_row_indices = find_black_rows(image, 94)
 black_col_indices = find_black_columns(image, 94)
 
+process_grid_blocks(image, black_row_indices, black_col_indices)
 
-# Process grid blocks
-grid = process_grid_blocks(image, black_row_indices, black_col_indices, 165)
-
-
+# Save the result
 cv2.imwrite("answer_python.png", image)
