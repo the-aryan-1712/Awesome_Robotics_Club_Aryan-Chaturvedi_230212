3fa8cebe9681231cba4a8333b61e780b0036dabc
Aryan Chaturvedi <aryanc23@iitk.ac.in>
2024-06-24 18:42:48 +0530

    made hello.cpp and update README.md

---
+++ hello.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define test(t) int t;cin>>t;while(t--)
+#define MOD 1000000007
+#define endl '\n' 
+#define PI 3.141592653589793238460
+typedef long long ll ;
+
+typedef set<pair<int,int>> spi;
+typedef set<pair<ll,ll>> spl;
+
+typedef vector<pair<int,int>> vpi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<string> vs;
+
+typedef map<int,int>mi;
+typedef map<ll,ll> ml;
+
+typedef set<string> ss;
+typedef set<char>sc;
+typedef set<int> si;
+typedef set<ll> sl;
+
+int main()
+{   
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    test(t){
+        
+
+    }
+    
+    
+    
+    
+    return 0;
+}
